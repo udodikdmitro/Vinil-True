42db2d17675d33a7980e86a040e423b18a749e2a
Mykola Melnyk <mykolamelnyk338@gmail.com>
2025-05-13 01:00:58 +0300

    Change id type in FileDataRepository to Long

--- src/main/java/com/vinylshop/repository/FileDataRepository.java
+++ src/main/java/com/vinylshop/repository/FileDataRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface FileDataRepository extends JpaRepository<FileData, UUID> {
+public interface FileDataRepository extends JpaRepository<FileData, Long> {
 
 }
